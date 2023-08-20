506546961f7e3cc3924428cc09cf52d09bf6fdc6
Tarun-108 <tarunstu125@gmail.com>
2023-08-20 15:00:44 +0530

    route authenticated for pics

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,9 @@
 const {Router} = require('express')
 const productsControllers = require('../controllers/productsControllers')
+const {authenticateJWT} = require("../middlewares/auth");
 const router = Router();
 
 router.post('/products/getAll', productsControllers.getAll);
-router.post('/products/pics', productsControllers.getRecommended);
+router.post('/products/pics', authenticateJWT, productsControllers.getRecommended);
 
 module.exports = router;
